2549e04b68e6984baa3bb349d8bca9ccc268f10d
Lucastini <47428112+Lucastini@users.noreply.github.com>
2019-06-10 05:57:31 -0300

    Add files via upload

---
+++ 8-6-19-ConeccionBaseDatos/src/negocio/Principal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocio;
+
+
+import presentacion.VentanaNuevoCliente;
+
+public class Principal {
+
+
+    public static void main(String[] args) {
+
+        VentanaNuevoCliente vnc=new VentanaNuevoCliente();
+        vnc.setVisible(true);
+        
+        
+        
+        
+    }
+
+    
+    
+}
